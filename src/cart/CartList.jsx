b71b73ae60535f0878cf7586e1edadbd13a81edd
Froggles <elmsterm@gmail.com>
2026-02-04 14:34:03 -0500

    sort cart by order item was added

--- src/cart/CartList.jsx
+++ src/cart/CartList.jsx
@@ -1,16 +1,12 @@
 import CartItem from "./CartItem";
 
-function sortCart(a, b) {
-  return a.id - b.id;
-}
-
 export default function CartList({ cart, addToCart, removeFromCart }) {
   if (cart.length < 1) {
     return <p>Add items to your cart</p>;
   }
   return (
     <ul>
-      {cart.sort(sortCart).map((item) => (
+      {cart.map((item) => (
         <CartItem
           key={item.id}
           plant={item}
